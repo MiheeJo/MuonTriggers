15551379ff7da5f2d7aefe38c456b4785a47fc51
miheejo <>
2011-10-10 15:35:04 +0000

    test/hltmutree_cfg.py

--- HLTMuTree/test/hltmutree_cfg.py
+++ HLTMuTree/test/hltmutree_cfg.py
@@ -6,8 +6,9 @@ process.load("FWCore.MessageService.MessageLogger_cfi")
 process.load('Configuration.StandardSequences.Services_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 
-#process.GlobalTag.globaltag = 'GR_R_41_V0::All'
-process.GlobalTag.globaltag = 'GR_R_42_V18::All'
+#process.GlobalTag.globaltag = 'GR_R_44_V4::All'
+#process.GlobalTag.globaltag = 'STARTHI44_V4::All'
+process.GlobalTag.globaltag = 'START44_V4::All'
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
@@ -15,30 +16,33 @@ process.source = cms.Source("PoolSource",
     noEventSort = cms.untracked.bool(True),
     duplicateCheckMode = cms.untracked.string('noDuplicateCheck'),
     fileNames = cms.untracked.vstring(
-      "rfio:/castor/cern.ch/user/m/mironov/cmssw424hltpatch1/HICorePhysics_L1SingleMu3_RAWHLTRECO/root/l1SingleMu3_f0.root",
+_input_
+#    "rfio:///castor/cern.ch/user/m/mironov/cmssw440patch10/MC425/jpsi/v3/jpsimc_rawrecohltdebug_83_1_Bxk.root",
     )
 )
 
 process.TFileService = cms.Service("TFileService",
-    fileName = cms.string("muTree_ups912.root")
+    fileName = cms.string(_output_)
+#    fileName = cms.string("test.root")
 )
 
 # load centrality
 from CmsHi.Analysis2010.CommonFunctions_cff import *
+'''
 overrideCentrality(process)
 process.HeavyIonGlobalParameters = cms.PSet(
   centralityVariable = cms.string("HFhits"),
   nonDefaultGlauberModel = cms.string(""),
   centralitySrc = cms.InputTag("hiCentrality")
 )
-
+'''
 
 process.analysis = cms.EDAnalyzer('HLTMuTree',
   muons = cms.InputTag("muons"),
   vertices = cms.InputTag("hiSelectedVertex"),
 #  vertices = cms.InputTag("offlinePrimaryVertices"),
   doReco = cms.untracked.bool(True),
-  doGen = cms.untracked.bool(False),
+  doGen = cms.untracked.bool(True),
   genparticle = cms.InputTag("hiGenParticles"),
   simtrack = cms.InputTag("mergedtruth","MergedTrackTruth"),
 )

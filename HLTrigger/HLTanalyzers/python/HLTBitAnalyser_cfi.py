39a0de373240c5f8cd85e8a5f0fc1ef99027c455
miheejo <>
2011-11-02 10:50:47 +0000

    *** empty log message ***

--- HLTrigger/HLTanalyzers/python/HLTBitAnalyser_cfi.py
+++ HLTrigger/HLTanalyzers/python/HLTBitAnalyser_cfi.py
@@ -4,13 +4,13 @@ hltbitanalysis = cms.EDAnalyzer("HLTBitAnalyzer",
     ### Trigger objects
     l1GctHFBitCounts                = cms.InputTag("hltGctDigis"),
     l1GctHFRingSums                 = cms.InputTag("hltGctDigis"),
-    l1GtObjectMapRecord             = cms.InputTag("hltL1GtObjectMap::HLT"),
-    l1GtReadoutRecord               = cms.InputTag("hltGtDigis::HLT"),
+    l1GtObjectMapRecord             = cms.InputTag("hltL1GtObjectMap::TEST"),
+    l1GtReadoutRecord               = cms.InputTag("hltGtDigis::TEST"),
 
     l1extramc                       = cms.string('hltL1extraParticles'),
     l1extramu                       = cms.string('hltL1extraParticles'),
-    hltresults                      = cms.InputTag("TriggerResults::HLT"),
-    HLTProcessName                  = cms.string("HLT"),
+    hltresults                      = cms.InputTag("TriggerResults::TEST"),
+    HLTProcessName                  = cms.string("TEST"),
                                 
     ### Run parameters
     RunParameters = cms.PSet(

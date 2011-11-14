932fc309ee2f5bb9a4516f2482d5bb1e3178d157
miheejo <>
2011-11-14 18:55:13 +0000

    bug fix on hltGetConfiguration

--- script/setupL1HLTreco44X.sh
+++ script/setupL1HLTreco44X.sh
@@ -4,7 +4,7 @@ cvs co -r $CMSSW_VERSION L1Trigger/Configuration/python
 cvs co -r $CMSSW_VERSION HLTrigger/Configuration/python
 
 # Get hlt menu (if necessary)
-hltGetConfiguration /users/miheejo/test1/442HIonV30/V1 --data --unprescale --cff --full --offline --process HLT > $CMSSW_BASE/src/HLTrigger/Configuration/python/HLT_HIon_l3Mu_cff.py 
+hltGetConfiguration /users/miheejo/test1/442HIonV30/V1 --data --unprescale --cff --offline --process HLT > $CMSSW_BASE/src/HLTrigger/Configuration/python/HLT_HIon_l3Mu_cff.py 
 
 # Get _cfg.py for running
 cvs co -d UserCode/test UserCode/Miheejo/MuTrig/test/

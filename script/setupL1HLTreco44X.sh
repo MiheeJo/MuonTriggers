52cd14d9270c4b5d389d06b68cc0aadb3f5ce35b
miheejo <>
2011-11-14 18:04:13 +0000

    setup for L1 emulator, HLT and reco

---
+++ script/setupL1HLTreco44X.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+# Get L1, HLT configuration
+cvs co -r $CMSSW_VERSION L1Trigger/Configuration/python
+cvs co -r $CMSSW_VERSION HLTrigger/Configuration/python
+
+# Get hlt menu (if necessary)
+hltGetConfiguration /users/miheejo/test1/442HIonV30/V1 --data --unprescale --cff --full --offline --process HLT > $CMSSW_BASE/src/HLTrigger/Configuration/python/HLT_HIon_l3Mu_cff.py 
+
+# Get _cfg.py for running
+cvs co -d UserCode/test UserCode/Miheejo/MuTrig/test/
+
+scram b

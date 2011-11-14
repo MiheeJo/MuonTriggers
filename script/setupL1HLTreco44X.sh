3120fa6de07f95a81af03396a9f32cb347136bc9
miheejo <>
2011-11-14 22:32:28 +0000

    setupL1HLTreco44X.sh

--- script/setupL1HLTreco44X.sh
+++ script/setupL1HLTreco44X.sh
@@ -3,10 +3,11 @@
 cvs co -r $CMSSW_VERSION L1Trigger/Configuration/python
 cvs co -r $CMSSW_VERSION HLTrigger/Configuration/python
 
-# Get hlt menu (if necessary)
-hltGetConfiguration /users/miheejo/test1/442HIonV30/V1 --data --unprescale --cff --offline --process TestHlt > $CMSSW_BASE/src/HLTrigger/Configuration/python/HLT_HIon_l3Mu_cff.py 
+# Get hlt menu (if necessary, try on interactively. not working with this option)
+#hltGetConfiguration /users/miheejo/test1/442HIonV30/V1 --data --unprescale --cff --offline --process TestHlt > $CMSSW_BASE/src/HLTrigger/Configuration/python/HLT_HIon_l3Mu_cff.py 
 
 # Get _cfg.py for running
 cvs co -d UserCode/test UserCode/Miheejo/MuTrig/test/
+cp UserCode/test/HLT_HIon_l3Mu_cff.py HLTrigger/Configuration/python
 
 scram b
